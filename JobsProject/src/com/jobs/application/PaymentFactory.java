655a7dfaba4d0fe698cee206a66ec449517d3d89
cieyj <cieyj@192.168.1.41>
2021-03-18 13:19:26 +0100

    Nivell 1

--- JobsProject/src/com/jobs/application/PaymentFactory.java
+++ JobsProject/src/com/jobs/application/PaymentFactory.java
@@ -18,7 +18,7 @@ public class PaymentFactory {
 		return new IPaymentRate() {
 			@Override
 			public double pay(double salaryPerMonth) {
-				return salaryPerMonth*0.75; 
+				return salaryPerMonth*0.85; 
 			}
 		};
 	}
